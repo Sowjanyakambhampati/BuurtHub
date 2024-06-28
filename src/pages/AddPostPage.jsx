2b6ec18bab0584c063ede181937148db845c8296
Sowjanya Kambhampati <sowjanyaakella87@gmail.com>
2024-06-28 08:34:57 +0200

    CreatedAt field added in Addpost page

--- src/pages/AddPostPage.jsx
+++ src/pages/AddPostPage.jsx
@@ -142,6 +142,17 @@ function AddPostPage() {
             />
           </div>
           <div className="grid grid-cols-2 gap-4">
+            <label htmlFor="createdAt" className="block text-sm font-medium text-gray-700 text-left">createdAt</label>
+            <textarea
+                id="createdAt"
+                name="createdAt"
+                value={post.createdAt}
+                onChange={handleChange}
+                required
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-4">
             <label htmlFor="image" className="block text-sm font-medium text-gray-700 text-left">Image:</label>
             <input
                 type="file"
