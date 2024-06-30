222072a2263b118ecfd250bc48614a4161f87349
Tetsewa <amatwilliams@gmail.com>
2024-06-30 16:05:43 +0200

    edit and delete function updated for products added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,20 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
-import {ToastContainer, toast} from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import SideNav from '../components/SideNav';
-import {CityContext} from '../context/CityContext';
+import { CityContext } from '../context/CityContext';
 
-function UserDashboard({session}) {
+function UserDashboard({ session }) {
     const [userProducts, setUserProducts] = useState([]);
     const [reservedProducts, setReservedProducts] = useState([]);
     const [registeredEvents, setRegisteredEvents] = useState([]);
     const [userPosts, setUserPosts] = useState([]);
     const [editProduct, setEditProduct] = useState(null);
-    const {selectedCity} = useContext(CityContext);
+    const { selectedCity } = useContext(CityContext);
 
-    const {user} = session;
+    const { user } = session;
+
     useEffect(() => {
         const fetchUserProducts = async () => {
             try {
@@ -25,7 +26,6 @@ function UserDashboard({session}) {
         };
         fetchUserProducts();
     }, [user.id]);
-
 
     useEffect(() => {
         const fetchUserReservedProducts = async () => {
@@ -39,13 +39,47 @@ function UserDashboard({session}) {
         fetchUserReservedProducts();
     }, [user.id]);
 
+    const handleEditChange = (e) => {
+        const { name, value } = e.target;
+        setEditProduct(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
 
+    const handleEditSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.put(`https://community-forum-backend.adaptable.app/product/${editProduct._id}`, editProduct);
+            toast.success('Product updated successfully');
+            setUserProducts(prevProducts =>
+                prevProducts.map(product =>
+                    product._id === editProduct._id ? response.data : product
+                )
+            );
+            setEditProduct(null);
+        } catch (error) {
+            toast.error('Failed to update product');
+            console.error('Failed to update product', error);
+        }
+    };
+
+    const handleDelete = async (productId) => {
+        try {
+            await axios.delete(`https://community-forum-backend.adaptable.app/product/${productId}`);
+            toast.success('Product deleted successfully');
+            setUserProducts(prevProducts => prevProducts.filter(product => product._id !== productId));
+        } catch (error) {
+            toast.error('Failed to delete product');
+            console.error('Failed to delete product', error);
+        }
+    };
 
     return (
         <div className="flex">
-            <ToastContainer/>
+            <ToastContainer />
             <div className="w-1/4">
-                <SideNav/>
+                <SideNav />
             </div>
             <div className="w-3/4 p-4">
                 <h2 className="text-2xl font-bold mb-4">User Dashboard</h2>
@@ -61,21 +95,65 @@ function UserDashboard({session}) {
                                             <div className="form-group">
                                                 <label className="block text-sm font-medium text-gray-700">Name:</label>
                                                 <input
-                                                    name="name"
-                                                    value={editProduct.name}
+                                                    name="productName"
+                                                    value={editProduct.productName}
                                                     onChange={handleEditChange}
                                                     className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                                 />
                                             </div>
                                             <div className="form-group">
-                                                <label
-                                                    className="block text-sm font-medium text-gray-700">Description:</label>
+                                                <label className="block text-sm font-medium text-gray-700">Description:</label>
                                                 <textarea
                                                     name="description"
                                                     value={editProduct.description}
                                                     onChange={handleEditChange}
                                                     className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                                                 />
+                                            </div>
+                                            <div className="form-group">
+                                                <label className="block text-sm font-medium text-gray-700">Price:</label>
+                                                <input
+                                                    name="price"
+                                                    type="number"
+                                                    value={editProduct.price}
+                                                    onChange={handleEditChange}
+                                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                                />
+                                            </div>
+                                            <div className="form-group">
+                                                <label className="block text-sm font-medium text-gray-700">Category:</label>
+                                                <select
+                                                    name="category"
+                                                    value={editProduct.category}
+                                                    onChange={handleEditChange}
+                                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                                >
+                                                    <option value="">Select a category</option>
+                                                    <option value="Furniture">Furniture</option>
+                                                    <option value="Electronics">Electronics</option>
+                                                    <option value="Utensils">Utensils</option>
+                                                    <option value="Clothing">Clothing</option>
+                                                    <option value="Kids">Kids</option>
+                                                    <option value="Pet Care">Pet Care</option>
+                                                    <option value="Entertainment">Entertainment</option>
+                                                    <option value="Sports">Sports</option>
+                                                    <option value="Appliances">Appliances</option>
+                                                </select>
+                                            </div>
+                                            <div className="form-group">
+                                                <label className="block text-sm font-medium text-gray-700">Condition:</label>
+                                                <select
+                                                    name="condition"
+                                                    value={editProduct.condition}
+                                                    onChange={handleEditChange}
+                                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                                >
+                                                    <option value="">Select a condition</option>
+                                                    <option value="New">New</option>
+                                                    <option value="Very Good">Very Good</option>
+                                                    <option value="Good">Good</option>
+                                                    <option value="Satisfactory">Satisfactory</option>
+                                                </select>
                                             </div>
                                             <button
                                                 type="submit"
@@ -93,11 +171,14 @@ function UserDashboard({session}) {
                                         </form>
                                     ) : (
                                         <>
-                                            <h4 className="text-lg font-semibold mb-2">{product.name}</h4>
-                                            <p className="text-gray-600 mb-2">{product.description}</p>
+                                            <h4 className="text-lg font-semibold mb-2">{product.productName}</h4>
+                                            <p className="text-gray-600 mb-2">Price: €{product.price}</p>
+                                            <p className="text-gray-600 mb-2">Description: {product.description}</p>
+                                            <p className="text-gray-600 mb-2">Category: {product.category}</p>
+                                            <p className="text-gray-600 mb-2">Condition: {product.condition}</p>
                                             {product.image && (
-                                                <img src={product.image} alt={product.name} className="mt-4 rounded-md"
-                                                     style={{maxWidth: '100%', height: 'auto'}}/>
+                                                <img src={product.image} alt={product.productName} className="mt-4 rounded-md"
+                                                    style={{ maxWidth: '100%', height: 'auto' }} />
                                             )}
                                             <div className="absolute bottom-4 left-4 flex space-x-2">
                                                 <button
@@ -124,50 +205,46 @@ function UserDashboard({session}) {
                 </section>
 
                 <section className="mb-8">
-                    <h3 className="text-xl font-semibold mb-2">Products Reserved by You</h3>
+                    <h3 className="text-xl font-semibold mb-2">Reserved Products</h3>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                         {reservedProducts.length > 0 ? (
                             reservedProducts.map(product => (
                                 <div key={product._id} className="bg-white p-4 rounded-lg shadow-md">
-                                    <h4 className="text-lg font-semibold mb-2">{product.name}</h4>
-                                    <p className="text-gray-600 mb-2">{product.description}</p>
+                                    <h4 className="text-lg font-semibold mb-2">{product.productName}</h4>
+                                    <p className="text-gray-600 mb-2">Price: €{product.price}</p>
+                                    <p className="text-gray-600 mb-2">Description: {product.description}</p>
+                                    <p className="text-gray-600 mb-2">Category: {product.category}</p>
+                                    <p className="text-gray-600 mb-2">Condition: {product.condition}</p>
                                     {product.image && (
-                                        <img src={product.image} alt={product.name} className="mt-4 rounded-md"
-                                             style={{maxWidth: '100%', height: 'auto'}}/>
+                                        <img src={product.image} alt={product.productName} className="mt-4 rounded-md"
+                                            style={{ maxWidth: '100%', height: 'auto' }} />
                                     )}
                                 </div>
                             ))
                         ) : (
-                            <p>No products reserved by you.</p>
+                            <p>No reserved products.</p>
                         )}
                     </div>
                 </section>
 
                 <section className="mb-8">
-                    <h3 className="text-xl font-semibold mb-2">Events Registered by You</h3>
+                    <h3 className="text-xl font-semibold mb-2">Registered Events</h3>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                         {registeredEvents.length > 0 ? (
                             registeredEvents.map(event => (
                                 <div key={event._id} className="bg-white p-4 rounded-lg shadow-md">
-                                    <h4 className="text-lg font-semibold mb-2">{event.title}</h4>
-                                    <p className="text-gray-600 mb-2">{event.description}</p>
-                                    <p className="text-gray-600 mb-2">Date: {new Date(event.date).toLocaleDateString()}</p>
-                                    <p className="text-gray-600 mb-2">City: {event.city}</p>
-                                    <p className="text-gray-600 mb-2">Location: {event.location}</p>
-                                    <p className="text-gray-600 mb-2">Organiser: {event.organiser}</p>
-                                    {event.image && (
-                                        <img src={event.image} alt={event.title} className="mt-4 rounded-md"
-                                             style={{maxWidth: '100%', height: 'auto'}}/>
-                                    )}
+                                    <h4 className="text-lg font-semibold mb-2">{event.name}</h4>
+                                    <p className="text-gray-600 mb-2">{event.date}</p>
+                                    <p className="text-gray-600 mb-2">{event.location}</p>
                                 </div>
                             ))
                         ) : (
-                            <p>No events registered by you.</p>
+                            <p>No registered events.</p>
                         )}
                     </div>
                 </section>
 
-                <section>
+                <section className="mb-8">
                     <h3 className="text-xl font-semibold mb-2">Your Posts</h3>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                         {userPosts.length > 0 ? (
