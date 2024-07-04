328cad6edd18344cb248108af0dada2e3f9e6f20
Tetsewa <amatwilliams@gmail.com>
2024-07-04 22:01:41 +0200

    updated homepage with styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,29 @@
-// HomePage.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import CitySelection from '../components/CitySelection';
 import Testimonials from '../components/Testimonials';
 
 function HomePage() {
+  useEffect(() => {
+    // Function to animate each step
+    function animateSteps() {
+      const steps = document.querySelectorAll('.step');
+      let delay = 0;
+
+      steps.forEach((step, index) => {
+        setTimeout(() => {
+          step.classList.remove('hidden');
+          step.classList.add('animate__animated', 'animate__fadeInUp'); // Add your animation class here
+        }, delay);
+        delay += 500; // Adjust this value for the desired animation delay
+      });
+    }
+
+    // Call the function to start animating steps after the page has loaded
+    animateSteps();
+  }, []); // Empty dependency array ensures this effect runs only once after initial render
+
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="min-h-screen">
       <div className="text-center py-8">
         <h1 className="text-4xl font-bold mb-4">Welcome to BuurtHub!</h1>
       </div>
@@ -15,18 +33,18 @@ function HomePage() {
         </div>
       </div>
       <div className="text-center py-8">
-        <h1 className="text-3xl font-bold mb-4">Why register on BuurtHub?</h1>
+        <h1 className="text-3xl font-bold mb-4">Why Register on BuurtHub?</h1>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-4">
         <section className="benefits bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-          <img className="w-full h-48 object-cover rounded-t-lg" src='/events.jpg' alt='events' />
-          <h3 className="text-xl font-semibold mt-4">Stay Informed and Engaged with Community Events and News</h3>
-          <p className="mt-2"><b>BuurtHub</b> keeps you up-to-date with the latest events and news in your community. Easily sign up for upcoming community events such as block parties, clean-up days, or local fairs. Also stay informed about important news and announcements from community.</p>
+          <img className="w-full h-48 center rounded-t-lg" src='/events.jpg' alt='events' />
+          <h3 className="text-xl font-semibold mt-4">Stay Informed and Engaged with Community Events</h3>
+          <p className="mt-2"><b>BuurtHub</b> keeps you up-to-date with the latest events and activities in your community. Easily sign up for upcoming community events and also stay informed about important announcements from community.</p>
         </section>
         <section className="benefits bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
           <img className="w-full h-48 object-cover rounded-t-lg" src='/discussion.jpg' alt='discussion' />
-          <h3 className="text-xl font-semibold mt-4">Participate in Meaningful Discussions and Build Connections</h3>
-          <p className="mt-2"><b>BuurtHub</b> provides a platform for topical discussions, allowing you to engage in conversations: Discuss a wide range of topics that matter to your community. From neighborhood safety to local businesses, share your thoughts and hear from others.</p>
+          <h3 className="text-xl font-semibold mt-4">Make Posts and Build Connections</h3>
+          <p className="mt-2"><b>BuurtHub</b> provides a platform which allows users to make posts about things in the community. From neighborhood safety to local businesses, share your thoughts and hear from others.</p>
         </section>
         <section className="benefits bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
           <img className="w-full h-48 object-cover rounded-t-lg" src='/marketplace.jpg' alt='marketplace' />
@@ -42,26 +60,24 @@ function HomePage() {
         <div className="md:w-1/2 mb-4 md:mb-0">
           <img src="/How_it_works.gif" alt="How it works" className="w-full h-auto rounded-lg shadow-lg" />
         </div>
-        <div className="md:w-1/2 flex flex-col space-y-4">
-          <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-            <h3 className="text-2xl font-semibold mb-2 text-left">1. Create an Account</h3>
-            <p className="mb-4">To get started, simply sign up for an account using your email address. Once registered, you can create a profile, add your location, and start interacting with your neighbors.</p>
-          </div>
-          <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-            <h3 className="text-2xl font-semibold mb-2 text-left">2. Browse and Post Products</h3>
-            <p className="mb-4">Explore a wide range of products listed by your neighbors. You can browse different categories, search for specific items, and contact sellers directly. If you have items you no longer need, you can easily list them for sale or give them away to others in your community.</p>
-          </div>
-          <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-            <h3 className="text-2xl font-semibold mb-2 text-left">3. Reserve and Pick Up Items</h3>
-            <p className="mb-4">See something you like? Reserve the item and arrange a pick-up time with the seller. BuurtHub makes it easy to coordinate with your neighbors and ensure a smooth exchange process.</p>
-          </div>
-          <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-            <h3 className="text-2xl font-semibold mb-2 text-left">4. Join and Create Events</h3>
-            <p className="mb-4">Stay updated with local events and activities. Whether it's a neighborhood cleanup, a community garage sale, or a social gathering, you can find events that interest you. Feel free to create your own events and invite your neighbors to join.</p>
-          </div>
-          <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-            <h3 className="text-2xl font-semibold mb-2 text-left">5. Share Posts and Announcements</h3>
-            <p>Use the platform to share important announcements, news, or requests with your community. Whether you’re looking for a recommendation or offering help, BuurtHub’s posting feature keeps everyone connected and informed.</p>
+        <div className="md:w-1/2 bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300" id="buurthub-steps" style={{ height: '550px' }}>
+          <h3 className="text-3xl font-bold mb-4 text-center">How BuurtHub Works</h3>
+          <div className="flex flex-col space-y-4">
+            <div className="step border-b border-gray-200 pb-4 hidden">
+              <h4 className="text-2xl font-semibold mb-2">1. Create an account using your email address or connect with your existing Google account</h4>
+            </div>
+            <div className="step border-b border-gray-200 pb-4 hidden">
+              <h4 className="text-2xl font-semibold mb-2">2. Browse through a wide range of products from various categories listed by your neighbours and add your own products for sale or give away</h4>
+            </div>
+            <div className="step border-b border-gray-200 pb-4 hidden">
+              <h4 className="text-2xl font-semibold mb-2">3. Reserve and pick up items that you reserve</h4>
+            </div>
+            <div className="step border-b border-gray-200 pb-4 hidden">
+              <h4 className="text-2xl font-semibold mb-2">4. Register for an event or also add event</h4>
+            </div>
+            <div className="step pb-4 hidden">
+              <h4 className="text-2xl font-semibold mb-2">5. Use the platform to share important announcements, news, or requests with your community</h4>
+            </div>
           </div>
         </div>
       </div>
